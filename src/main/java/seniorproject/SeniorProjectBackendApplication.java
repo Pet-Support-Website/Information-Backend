af9154fa130fcb665edc9e6b8c5ed1da03d83b8f
kittisak <kittisak_kae@cmu.ac.th>
2023-06-30 15:48:30 +0700

    first commit
    init project

---
+++ src/main/java/seniorproject/SeniorProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package seniorproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeniorProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seniorproject/SeniorProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package seniorproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
